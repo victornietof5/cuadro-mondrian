d2a7629e0056a4641555ca5776cf877545b76307
victornietof5 <vihugo317f5@gmail.com>
2023-03-29 13:59:18 +0200

    add new animation

--- script.js
+++ script.js
@@ -18,3 +18,42 @@ $(document).ready(function() {
         }
     });
 });
+
+$(document).ready(function() {
+    setInterval(function() {
+        $('.box').each(function() {
+            if (!$(this).hasClass('animated')) {
+                $(this).animate({
+                    left: '+=500px'
+                }, 500).addClass('animated');
+            } else {
+                $(this).animate({
+                    left: '-=500px'
+                }, 500).removeClass('animated');
+            }
+        });
+    }, 8000);
+});
+
+$(document).ready(function() {
+    let currentIndex = 0;
+    let boxes = $('.box');
+
+    setInterval(function() {
+        let currentBox = $(boxes[currentIndex]);
+        if (!currentBox.hasClass('animated')) {
+            currentBox.animate({
+                left: '+=500px'
+            }, 100).addClass('animated');
+        } else {
+            currentBox.animate({
+                left: '-=500px'
+            }, 100).removeClass('animated');
+        }
+
+        currentIndex++;
+        if (currentIndex >= boxes.length) {
+            currentIndex = 0;
+        }
+    }, 200);
+});
